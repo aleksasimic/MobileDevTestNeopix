f7d4baebd838f0d92dbfe84d697631ff8cf6db8d
Aleksa Simic <aleksa.sima.simic96@gmail.com>
2020-04-13 19:32:10 +0200

    Minor fixes and updates on order status label
    - Updated text color to white
    - Updated insets

---
+++ MobileDevTestNeopix/Common/Extensions/UILabel+Utilites.swift
@@ -0,0 +1,27 @@
+import Foundation
+import UIKit
+
+class InsetLabel: UILabel {
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: UIEdgeInsets(top: 0, left: 6, bottom: 0, right: 6)))
+    }
+}
+
+@IBDesignable class PaddingLabel: UILabel {
+
+    @IBInspectable var topInset: CGFloat = 5.0
+    @IBInspectable var bottomInset: CGFloat = 5.0
+    @IBInspectable var leftInset: CGFloat = 7.0
+    @IBInspectable var rightInset: CGFloat = 7.0
+
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + leftInset + rightInset,
+                      height: size.height + topInset + bottomInset)
+    }
+}
